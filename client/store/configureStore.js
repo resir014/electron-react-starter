2d3c97a5ab4356066eb23f5b8306dc27d5893d55
Resi Respati <resir014@gmail.com>
2016-10-21 10:37:54 +0700

    Included configureStore settings for different environment vars

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,37 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { hashHistory } from 'react-router'
-import { routerMiddleware, push } from 'react-router-redux'
-import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
-import rootReducer from '../reducers'
-
-import * as counterActions from '../actions/counter'
-
-const actionCreators = {
-  ...counterActions,
-  push
-}
-
-const logger = createLogger({
-  level: 'info',
-  collapsed: true
-})
-
-const router = routerMiddleware(hashHistory)
-
-const enhancer = compose(
-  applyMiddleware(thunk, router, logger),
-  window.devToolsExtension
-    ? window.devToolsExtension({ actionCreators })
-    : (noop) => { return noop }
-)
-
-export default function configureStore (initialState) {
-  const store = createStore(rootReducer, initialState, enhancer)
-
-  if (window.devToolsExtension) {
-    window.devToolsExtension.updateStore(store)
-  }
-
-  return store
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod')
+} else {
+  module.exports = require('./configureStore.dev')
 }
