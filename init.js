12da6dda29e069627d787cb01280e1ed52905213
Resi Respati <resir014@gmail.com>
2016-10-21 11:24:34 +0700

    Use `process.cwd()` instead of `__dirname`

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/public/index.html`)
+  win.loadURL(`file://${process.cwd()}/public/index.html`)
 
   // Open the DevTools.
   win.webContents.openDevTools()
