deac108a1c83f32ed422da9a52e5c9b8d048b444
Resi Respati <resir014@gmail.com>
2016-10-22 15:54:21 +0700

    Added the Dedupe and OccurenceOrder plugins

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,8 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ]
 }
